ef7af4ff693fa90c84729ddd9d16daa294505d50
Dave Snider <dave.snider@gmail.com>
2013-10-21 10:25:24 -0700

    fix click events in mobile menu

--- docs/source/_themes/wyrm_theme/static/wyrm_theme.js
+++ docs/source/_themes/wyrm_theme/static/wyrm_theme.js
@@ -1,6 +1,11 @@
 $( document ).ready(function() {
-  $(".icon-reorder").click(function () {
-    $(".wy-nav-content-wrap").toggleClass("shift");
-    $(".wy-nav-side").toggleClass("shift");
+  // Shift nav in mobile when clicking the menu.
+  $("[data-toggle='wy-nav-top']").click(function() {
+    $("[data-toggle='wy-nav-shift']").toggleClass("shift");
+    window.shifted = true;
+  });
+  // Close menu when you click a link.
+  $(".wy-menu-vertical .current li").click(function() {
+    $("[data-toggle='wy-nav-shift']").removeClass("shift");
   });
 });
