eefdae7adf5e2d8a3ec6ce7c5ea1aadb4f0a1445
Dave Snider <dave.snider@gmail.com>
2014-01-15 12:42:32 -0800

    remove theme from here

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -108,7 +108,7 @@ html_theme = 'wyrm_theme'
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = ["_themes/sphinx_rtd_theme", ]
-html_theme_path = ["_themes", ]
+# html_theme_path = ["_themes", ]
 
 
 # The name for this set of Sphinx documents.  If None, it defaults to
